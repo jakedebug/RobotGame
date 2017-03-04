7493dd49d139de2f5bfb4331ba04f24e2269bd8c
jakedebug <jakedebug@gmail.com>
2017-03-04 17:52:43 +0000

    initial commit

---
+++ desktop/src/com/jakedebug/games/robotgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jakedebug.games.robotgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jakedebug.games.robotgame.RobotGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RobotGame(), config);
+	}
+}
