4ef637cbaff45ab3c3c2a19c52c2633089e1c3e5
jakedebug <jakedebug@gmail.com>
2017-03-04 18:00:13 +0000

    test commit

--- core/src/com/jakedebug/games/robotgame/RobotGame.java
+++ core/src/com/jakedebug/games/robotgame/RobotGame.java
@@ -9,7 +9,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class RobotGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
+	//test
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
